6bf659648e1012680c758ddb29cf9a07aa82b829
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-03-07 16:38:03 -0500

    Initial Commit

---
+++ QuartzFun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuartzFun
+//
+//  Created by Tom Tsiliopoulos on 2017-03-07.
+//  Copyright © 2017 Tom Tsiliopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
